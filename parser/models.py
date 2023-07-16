ab6f883d040b0a8dfa804dd5765f4d7918fda79c
a.baranovsky <aleksey.baranovsky@gmail.com>
2023-07-16 17:48:21 +0300

    Create Data model

--- parser/models.py
+++ parser/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Data(models.Model):
+    row_id = models.IntegerField()
+    company = models.CharField(max_length=100)
+    fact_qliq_data1 = models.IntegerField()
+    fact_qliq_data2 = models.IntegerField()
+    fact_qoil_data1 = models.IntegerField()
+    fact_qoil_data2 = models.IntegerField()
+    forecast_qliq_data1 = models.IntegerField()
+    forecast_qliq_data2 = models.IntegerField()
+    forecast_qoil_data1 = models.IntegerField()
+    forecast_qoil_data2 = models.IntegerField()
+    on_date = models.DateField()
