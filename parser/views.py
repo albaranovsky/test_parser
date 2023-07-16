8fb790bb3a067b971e85c9458fb6d7e3d3dc3bec
a.baranovsky <aleksey.baranovsky@gmail.com>
2023-07-16 18:47:06 +0300

    Add index view

--- parser/views.py
+++ parser/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Data
+from .parser import add_date_column, open_sheet, parse_sheet
+
+
+def index(request):
+    if request.method == 'POST':
+        sheet = open_sheet(request.FILES['xls_file'])
+        data = parse_sheet(sheet)
+        add_date_column(data)
+        Data.objects.all().delete()
+        for item in data:
+            Data.objects.create(**item)
+
+    return render(request, 'parser/index.html')
