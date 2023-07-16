72d02b118f2788c815492ae30539c534bd5ee7a4
a.baranovsky <aleksey.baranovsky@gmail.com>
2023-07-16 23:14:24 +0300

    Add errors handler

--- parser/views.py
+++ parser/views.py
@@ -5,17 +5,21 @@ from .parser import add_date_column, open_sheet, parse_sheet
 
 
 def index(request):
+    err_message = ''
     if request.method == 'POST':
-        sheet = open_sheet(request.FILES['xls_file'])
-        data = parse_sheet(sheet)
-        add_date_column(data)
-        Data.objects.all().delete()
-        for item in data:
-            Data.objects.create(**item)
+        try:
+            sheet = open_sheet(request.FILES['xls_file'])
+            data = parse_sheet(sheet)
+            add_date_column(data)
+            Data.objects.all().delete()
+            for item in data:
+                Data.objects.create(**item)
+        except Exception as e:
+            err_message = str(e)
+        else:
+            return redirect('result')
 
-        return redirect('result')
-
-    return render(request, 'parser/index.html')
+    return render(request, 'parser/index.html', {'err_message': err_message})
 
 
 def result(request):
