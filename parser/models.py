892ecdcf52a2671b278dcaae9c5858a2dd776e47
a.baranovsky <aleksey.baranovsky@gmail.com>
2023-07-16 19:55:08 +0300

    Add totals manager

--- parser/models.py
+++ parser/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from django.db.models import Sum
+
+
+class TotalManager(models.Manager):
+
+    def get_queryset(self):
+        return super().get_queryset().values('on_date').annotate(
+            fact_qliq_sum=(Sum('fact_qliq_data1') + Sum('fact_qliq_data2')),
+            fact_qoil_sum=(Sum('fact_qoil_data1') + Sum('fact_qoil_data2')),
+            forecast_qliq_sum=(
+                    Sum('forecast_qliq_data1') + Sum('forecast_qliq_data2')),
+            forecast_qoil_sum=(
+                    Sum('forecast_qoil_data1') + Sum('forecast_qoil_data2'))
+        )
 
 
 class Data(models.Model):
@@ -13,3 +27,6 @@ class Data(models.Model):
     forecast_qoil_data1 = models.IntegerField()
     forecast_qoil_data2 = models.IntegerField()
     on_date = models.DateField()
+
+    objects = models.Manager()
+    totals = TotalManager()
