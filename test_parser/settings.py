5eb23d70d5349ea12b9aab8d1fcefed7e753d9f3
albaranovsky <aleksey.baranovsky@gmail.com>
2023-07-18 15:44:13 +0300

    Add setting for date col algo

--- test_parser/settings.py
+++ test_parser/settings.py
@@ -1,3 +1,4 @@
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -110,3 +111,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Algo for generating date column ('random' or 'company'). Default: 'company'
+DATE_COL_ALGO = 'random'
